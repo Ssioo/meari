84ac992b91b24244979c452f0e359f6c1ce14a5f
yeonwoo_sio <wooisso@naver.com>
2020-09-30 18:14:11 +0900

    change server url

--- src/infra/constants.ts
+++ src/infra/constants.ts
@@ -1,7 +1,7 @@
 import { Dimensions, Platform } from 'react-native'
 
 export const SERVER_BASE_URL = __DEV__
-  ? 'http://localhost:3000/meari'
+  ? 'http://18.224.20.121:3000/meari'//'http://localhost:3000/meari'
   : 'http://18.224.20.121:3000/meari'
 
 export const WINDOW_WIDTH = Dimensions.get('window').width
